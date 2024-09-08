2832cf5d734059b1e4bdb053473ea48b97b8ad9f
Matias Barboza <matiasbarboza540@gmail.com>
2024-09-07 21:48:36 -0300

    feat: Comienzo lógica de usuario

---
+++ Gimnasio-Dominio/Usuario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gimnasio_Dominio
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Password { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public bool EsAdmin { get; set; }
+        public bool EsProfesor { get; set; }
+
+        public Usuario() 
+        {
+        }
+
+        public Usuario(int id, string nombreUsuario, string password, string nombre, string apellido, bool esAdmin, bool esProfesor)
+        {
+            Id = id;
+            NombreUsuario = nombreUsuario;
+            Password = password;
+            Nombre = nombre;
+            Apellido = apellido;
+            EsAdmin = esAdmin;
+            EsProfesor = esProfesor;
+        }
+    }
+}
---
+++ Gimnasio-Negocio/UsuarioNegocio.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gimnasio_Dominio;
+using Gimnasio_AccesoDatos;
+using System.Data;
+using Gimnasio_AccesoDatos.DataSetGimnasioTableAdapters;
+
+namespace Gimnasio_Negocio
+{
+    public class UsuarioNegocio
+    {
+        public bool ExisteUsuario(Usuario usuario) 
+        {
+            UsuariosTableAdapter usuariosTableAdapter = new UsuariosTableAdapter();
+            DataTable usuariosDataTable = usuariosTableAdapter.ExisteUsuario(usuario.NombreUsuario);
+
+            return usuariosDataTable.Rows.Count == 1;
+        }
+
+        public Usuario ObtenerUsuarioPor(string nombreUsuario, string password) 
+        {
+            UsuariosTableAdapter usuariosTableAdapter = new UsuariosTableAdapter();
+            DataTable usuariosDataTable = usuariosTableAdapter.ObtenerUsuarioPor(nombreUsuario, password);
+            Usuario usuarioBuscado = null;
+
+            if (usuariosDataTable.Rows.Count == 0) 
+            {
+                return usuarioBuscado;   
+            }
+
+            DataSetGimnasio.UsuariosRow usuarioFila = (DataSetGimnasio.UsuariosRow) usuariosDataTable.Rows[0];
+
+            usuarioBuscado = new Usuario(
+                usuarioFila.id,
+                usuarioFila.nombre_usuario,
+                usuarioFila.password,
+                usuarioFila.nombre,
+                usuarioFila.apellido,
+                usuarioFila.es_admin,
+                usuarioFila.es_profesor
+                );
+
+            usuariosTableAdapter.Dispose();
+            usuariosDataTable.Dispose();
+
+            return usuarioBuscado;
+        }
+
+        public Usuario ObtenerUsuarioPorId(int id)
+        {
+            UsuariosTableAdapter usuariosTableAdapter = new UsuariosTableAdapter();
+            DataTable usuariosDataTable = usuariosTableAdapter.ObtenerUsuarioPorId(id);
+            Usuario usuarioBuscado = null;
+
+            if (usuariosDataTable.Rows.Count == 0) 
+            {
+                return usuarioBuscado;
+            }
+
+            DataSetGimnasio.UsuariosRow usuarioFila = (DataSetGimnasio.UsuariosRow) usuariosDataTable.Rows[0];
+
+            usuarioBuscado = new Usuario(
+                usuarioFila.id,
+                usuarioFila.nombre_usuario,
+                usuarioFila.password,
+                usuarioFila.nombre,
+                usuarioFila.apellido,
+                usuarioFila.es_admin,
+                usuarioFila.es_profesor
+                );
+
+            usuariosTableAdapter.Dispose();
+            usuariosDataTable.Dispose();
+
+            return usuarioBuscado;
+        }
+
+        public List<Usuario> ObtenerUsuarios() 
+        {
+            UsuariosTableAdapter usuariosTableAdapter = new UsuariosTableAdapter();
+            DataTable usuariosDataTable = usuariosTableAdapter.ObtenerUsuarios();
+            List<Usuario> listaUsuarios = new List<Usuario>();
+
+            foreach (DataSetGimnasio.UsuariosRow usuarioFila in usuariosDataTable.Rows) 
+            {
+                Usuario usuario = new Usuario(
+                    usuarioFila.id,
+                    usuarioFila.nombre_usuario,
+                    usuarioFila.password,
+                    usuarioFila.nombre,
+                    usuarioFila.apellido,
+                    usuarioFila.es_admin,
+                    usuarioFila.es_profesor
+                    );
+
+                listaUsuarios.Add(usuario);
+            }
+
+            usuariosTableAdapter.Dispose();
+            usuariosDataTable.Dispose();
+
+            return listaUsuarios;
+        }
+
+        //-------------------------------------------------- OTRAS FUNCIONALIDADES ----------------------------------------------------------------------------
+        public bool InformacionCorrectaUsuario(Usuario usuarioAComprobar) 
+        {
+            return ObtenerUsuarioPor(usuarioAComprobar.NombreUsuario, usuarioAComprobar.Password) != null;
+        }
+    }
+}
