4d909498878a6f54302f79c85e86e4e1a18d66b9
Matias Barboza <matiasbarboza540@gmail.com>
2024-10-19 20:58:51 -0300

    refactory: Eliminacion de constructor no utilizado y agregado atributo

--- Gimnasio-Dominio/Cuota.cs
+++ Gimnasio-Dominio/Cuota.cs
@@ -15,24 +15,13 @@ namespace Gimnasio_Dominio
         public DateTime FechaVencimiento { get; set; }
         public string MesQueAbona { get; set; }
         public decimal MontoAbonado { get; set; }
+        public decimal ValorTipoCuota { get; set; }
         public bool Visible { get; set; }
 
         public Cuota() 
         {
             Socio = new Socio();
             TipoCuota = new TipoCuota();
-        }
-
-        public Cuota(int id, Socio socio, TipoCuota tipoCuota, DateTime fechaPago, DateTime fechaVencimiento, string mesQueAbona, decimal montoAbonado, bool visible)
-        {
-            Id = id;
-            Socio = socio;
-            TipoCuota = tipoCuota;
-            FechaPago = fechaPago;
-            FechaVencimiento = fechaVencimiento;
-            MesQueAbona = mesQueAbona;
-            MontoAbonado = montoAbonado;
-            Visible = visible;
         }
     }
 }
