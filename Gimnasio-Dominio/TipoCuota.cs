03fd2d2caa46c29cf8780d9f1f150f4c2412a345
Matias Barboza <matiasbarboza540@gmail.com>
2024-10-18 21:39:50 -0300

    refactory: Añadido de atributo a modelo y coreccion de constructor

--- Gimnasio-Dominio/TipoCuota.cs
+++ Gimnasio-Dominio/TipoCuota.cs
@@ -11,15 +11,17 @@ namespace Gimnasio_Dominio
         public int Id { get; set; }
         public string Descripcion { get; set; }
         public decimal Valor { get; set; }
+        public int CantidadEnDias { get; set; }
         public bool Visible { get; set; }
 
         public TipoCuota() { }
 
-        public TipoCuota(int id, string descripcion, decimal valor, bool visible)
+        public TipoCuota(int id, string descripcion, decimal valor, int cantidadDias, bool visible)
         {
             Id = id;
             Descripcion = descripcion;
             Valor = valor;
+            CantidadEnDias = cantidadDias;
             Visible = visible;
         }
     }
