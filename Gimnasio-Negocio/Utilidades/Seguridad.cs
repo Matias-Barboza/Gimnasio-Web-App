43052065d8e07c38ef4a2d8d4efc5e6a20d23973
Matias Barboza <matiasbarboza540@gmail.com>
2024-10-24 23:46:38 -0300

    feat: Añadido de encriptacion de pass y comprobacion tipo usuario

---
+++ Gimnasio-Negocio/Utilidades/Seguridad.cs
@@ -0,0 +1,40 @@
+﻿using Gimnasio_Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gimnasio_Utilidades
+{
+    public class Seguridad
+    {
+        public static string GetSHA256(string cadena) 
+        {
+            SHA256 hash = SHA256Managed.Create();
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] stream = null;
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stream = hash.ComputeHash(encoding.GetBytes(cadena));
+
+            for (int i = 0; i < stream.Length; i++) 
+            {
+                stringBuilder.AppendFormat("{0:x2}", stream[i]);
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        public static bool UsuarioEsAdmin(Usuario usuario)
+        {
+            return usuario.EsAdmin;
+        }
+
+        public static bool UsuarioEsProfesor(Usuario usuario)
+        {
+            return usuario.EsProfesor;
+        }
+    }
+}
